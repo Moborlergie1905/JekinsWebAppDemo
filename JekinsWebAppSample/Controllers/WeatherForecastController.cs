8a6677bef653becbdf2494ca5e47252aae28e357
IdealSoftTech <moborlergie1905@gmail.com>
2023-01-14 11:48:32 +0100

    Added xUnit test project

--- JekinsWebAppSample/Controllers/WeatherForecastController.cs
+++ JekinsWebAppSample/Controllers/WeatherForecastController.cs
@@ -13,10 +13,10 @@ namespace JekinsWebAppSample.Controllers
 
         private readonly ILogger<WeatherForecastController> _logger;
 
-        public WeatherForecastController(ILogger<WeatherForecastController> logger)
-        {
-            _logger = logger;
-        }
+        //public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        //{
+        //    _logger = logger;
+        //}
 
         [HttpGet(Name = "GetWeatherForecast")]
         public IEnumerable<WeatherForecast> Get()
---
+++ TestProject1/UnitTest1.cs
@@ -0,0 +1,15 @@
+using JekinsWebAppSample.Controllers;
+
+namespace TestProject1
+{
+    public class UnitTest1
+    {
+        WeatherForecastController forcast = new WeatherForecastController();
+        [Fact]
+        public void DoesListContain()
+        {
+            var list = forcast.Get().ToArray();
+            Assert.Contains(list, item => item.Summary != "Bolaji");
+        }
+    }
+}
